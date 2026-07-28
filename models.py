b281f31e4b3661a18ef0437fee7ddd9b6707cc9a
Amady <amady6.add@gmail.com>
2026-07-28 23:56:34 +0200

    initial commit

---
+++ models.py
@@ -0,0 +1,6 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Flatten
+# from tensorflow.keras.datasets import mnist
