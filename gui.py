452a6e9f761def908d1bfdca3777b4bdd1be89c8
Amady <amady6.add@gmail.com>
2026-07-29 01:48:48 +0200

    feat: gui creation (WIP)

---
+++ gui.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import numpy as np
+import cv2
+
+app = Tk()
+app.geometry("400x400")
+
+canvas = np.ones((280, 280), dtype=np.uint8) * 255
+print("\nDraw a digit on the image and it will predict the number.")
+print("Use an image editing tool to draw, then upload the image.")
+# Save and display the blank canvas (so you can draw on it)
+filename = "draw_digit.png"
+cv2.imwrite(filename, canvas)
+
+def get_x_and_y(event):
+    global lasx, lasy
+    lasx, lasy = event.x, event.y
+
+def draw_smth(event):
+    global lasx, lasy
+    canvas.create_line((lasx, lasy, event.x, event.y), fill='black', width=5)
+    lasx, lasy = event.x, event.y
+    
+
+canvas = Canvas(app, bg='black')
+canvas.pack(anchor='nw', fill='both', expand=1)
+
+canvas.bind("<Button-1>", get_x_and_y)
+canvas.bind("<B1-Motion>", draw_smth)
+
+
+image = Image.open("draw_digit.png")
+image = image.resize((400,400), Image.Resampling.LANCZOS)
+image = ImageTk.PhotoImage(image)
+canvas.create_image(0,0, image=image, anchor='nw')
+
+predict = Button(app, text="Predict")
+predict.pack()
+
+cancel = Button(app, text="Cancel")
+cancel.pack()
+
+
+app.mainloop()
